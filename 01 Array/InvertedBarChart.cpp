3d78d3421c57719f7d3f74fc8501c3d151c2a4f2
Aditya Sharma <adityasharma9319635@gmail.com>
2022-08-25 23:56:51 +0530

    Invertd Bar Chart

---
+++ 01 Array/InvertedBarChart.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int* arr = new int[n];
+    for(int idx = 0; idx <= n; idx++)
+    {
+        cin>>arr[idx];
+    }
+    
+    int max = arr[n];
+    for(int idx = 0; idx < n; idx++)
+    {
+        if(arr[idx] > max)
+        {
+            max = arr[idx];
+        }
+    }
+
+
+    for ( int col = 0; col  < max ; col++)
+    {
+        for ( int row = 0;  row < n; row++)
+        {
+            if(col < arr[row])
+            {
+                cout<<"*\t";
+            }
+            else{
+                cout<<"	";
+            }
+        }
+        cout<<endl;
+    }
+    
+    
+return 0;
+    
+}
