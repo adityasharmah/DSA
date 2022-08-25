6f5976c3f304eeddac8b794039b08b7394dacd62
Aditya Sharma <adityasharma9319635@gmail.com>
2022-08-25 23:47:53 +0530

    Bar Chart

---
+++ 01 Array/BarChart.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int* arr = new int[n];
+    for(int idx = 0; idx <= n; idx++)
+    {
+        cin>>arr[idx];
+    }
+    
+    int max = arr[0];
+    for(int idx = 1; idx < n; idx++)
+    {
+        if(arr[idx] > max)
+        {
+            max = arr[idx];
+        }
+    }
+    
+    for ( int col = max; col  >= 1; col--)
+    {
+        for ( int row = 0;  row < n; row++)
+        {
+            if(arr[row] >= col)
+            {
+                cout<<"*	";
+            }
+            else{
+                cout<<"	";
+            }
+        }
+        cout<<endl;
+    }
+    
+    
+return 0;
+    
+}
