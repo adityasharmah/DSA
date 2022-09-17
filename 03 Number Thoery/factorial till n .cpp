d8cd199a5b4d94006c5fa2c455bedfe459a0957d
Aditya Sharma <adityasharma9319635@gmail.com>
2022-09-18 02:08:59 +0530

    factorial till n

---
+++ 03 Number Thoery/factorial till n .cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact = 1;
+    while(n != 0)
+    { 
+       fact = fact * n;
+        n--;
+    }
+    return fact;
+}
+
+
+
+int main() {
+	int n;
+    cin >> n;
+
+    int result = factorial(n);
+    cout << result;
+	return 0;
+}
+
