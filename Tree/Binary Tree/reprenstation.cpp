ec4b0d7f57e9b6859784e27865371174ed8871cf
Aditya Sharma <adityasharma9319635@gmail.com>
2022-12-29 15:58:29 +0530

    tree

---
+++ Tree/Binary Tree/reprenstation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>        //
+using namespace std;            
+
+// using structure:
+struct node{
+    int data;                   // data
+    struct node *left;          // left child
+    struct node *right;         // right child
+};
+
+
+// using class:
+class Node{
+    public :                    // access specifier
+    int data;                   // data
+    Node *left;                 // left child
+    Node *right;                // right child
+};
+
+int main (){
+    cout << "ADITYA SHARMA 203-IT-B" << endl;
+}
+
