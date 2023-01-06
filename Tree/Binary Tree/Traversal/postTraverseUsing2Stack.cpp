7f010c2d13742dff3dd6342b2cce4c392aed1f1a
Aditya Sharma <adityasharma9319635@gmail.com>
2023-01-06 23:58:24 +0530

    -

--- Tree/Binary Tree/Traversal/postTraverseUsing2Stack.cpp
+++ Tree/Binary Tree/Traversal/postTraverseUsing2Stack.cpp
@@ -2,25 +2,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Structure of a tree node
 struct node {
   int data;
   struct node * left, * right;
 };
 
+// Function to perform post order traversal of the tree
 void postOrderTrav(node * curr, vector < int > & postOrder) {
   if (curr == NULL)
     return;
 
-  postOrderTrav(curr -> left, postOrder);
-  postOrderTrav(curr -> right, postOrder);
-  postOrder.push_back(curr -> data);
+  postOrderTrav(curr -> left, postOrder);              // Traverse left subtree
+  postOrderTrav(curr -> right, postOrder);             // Traverse right subtree
+  postOrder.push_back(curr -> data);                   // Print the root
 }
 
+// Utility function to create a new tree node
 struct node * newNode(int data) {
-  struct node * node = (struct node * ) malloc(sizeof(struct node));
-  node -> data = data;
-  node -> left = NULL;
-  node -> right = NULL;
+  struct node * node = (struct node * ) malloc(sizeof(struct node));    // Allocate memory
+  node -> data = data;                                            // Assign data to this node
+  node -> left = NULL;                                            // Initialize left and right children as NULL
+  node -> right = NULL;                                           // Initialize left and right children as NULL
 
   return (node);
 }
