15763cf7ca9a9731ccbb2e0f1037277ae4ba5aed
Aditya Sharma <adityasharma9319635@gmail.com>
2023-01-15 01:01:53 +0530

    same Tree

---
+++ Tree/Binary Tree/Traversal/sameTree.cpp
@@ -0,0 +1,25 @@
+// Same Tree
+// https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL &&  q == NULL) return true;                 // both are null hence same
+        if(p == NULL ||  q == NULL) return false;                // both are not null hence not same
+        if(p->val != q->val) return false;                       // both are not null and values are not same hence not same
+
+        // both are not null and values are same hence check for left and right subtrees
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left) ;  
+    }
+};
