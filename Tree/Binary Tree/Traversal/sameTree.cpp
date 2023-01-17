edff3cd9e18bf9a48490ecc0d072148cba2011e2
Aditya Sharma <adityasharma9319635@gmail.com>
2023-01-17 23:59:49 +0530

    -

--- Tree/Binary Tree/Traversal/sameTree.cpp
+++ Tree/Binary Tree/Traversal/sameTree.cpp
@@ -12,6 +12,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // 
+ //
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
