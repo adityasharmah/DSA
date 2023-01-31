f706663b8a29516de967d204b742ef6f1582a4b3
Aditya Sharma <adityasharma9319635@gmail.com>
2023-01-31 23:56:58 +0530

    practice

---
+++ 04 Recursion/factorial.cpp
@@ -0,0 +1,23 @@
+// factorials using recursion
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    int fnm1 = factorial(n-1);
+    int fn = n*fnm1;
+    return fn;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<factorial(n);
+    return 0;
+}
+
