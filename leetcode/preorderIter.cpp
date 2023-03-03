58c22ef91a9216a8db534305ee27c69cf506ffb8
Aditya Sharma <adityasharma9319635@gmail.com>
2023-03-03 23:59:24 +0530

    Preorder Traversal Iterative Approach

--- leetcode/preorderIter.cpp
+++ leetcode/preorderIter.cpp
@@ -9,15 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// leetcode 144. Binary Tree Preorder Traversal
+// Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+// Iterative solution for preorder traversal 
 class Solution {
 public:
     vector<int> arr;
     vector<int> preorderTraversal(TreeNode* root) {
+        // root, left, right
         if(root == NULL)
              return arr;
         
+        // Create a stack
         stack<TreeNode*> st;
-        st.push(root);
+        st.push(root);                                     // Push the root node into the stack
         while(!st.empty())
         {
             root = st.top();
