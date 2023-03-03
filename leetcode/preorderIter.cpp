770a9c7040b30283d1e7d1374af167e7110a851a
Aditya Sharma <adityasharma9319635@gmail.com>
2023-03-03 23:59:57 +0530

    Preorder Traversal Iterative Approach

--- leetcode/preorderIter.cpp
+++ leetcode/preorderIter.cpp
@@ -26,9 +26,11 @@ public:
         st.push(root);                                     // Push the root node into the stack
         while(!st.empty())
         {
-            root = st.top();
-            st.pop();
-            arr.push_back(root -> val);
+            
+            root = st.top();                              // Get the top element of the stack
+            st.pop();                                     // Pop the top element of the stack
+            arr.push_back(root -> val);                   // Push the value of the root node into the array
+            
             if(root -> right != NULL)
             {
                 st.push(root -> right);
